67a63aa4dc2088b3510bd85925ec2bbef0ede740
Jeroen De Meerleer <me@jeroened.be>
2017-04-15 13:50:51 +0200

    Added database

---
+++ database.sql
@@ -0,0 +1,76 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Gegenereerd op: 15 apr 2017 om 06:49
+-- Serverversie: 5.5.52-MariaDB
+-- PHP-versie: 7.1.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `jeroened_webcron`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `jobs`
+--
+
+DROP TABLE IF EXISTS `jobs`;
+CREATE TABLE IF NOT EXISTS `jobs` (
+  `jobID` int(11) NOT NULL AUTO_INCREMENT,
+  `user` int(11) NOT NULL,
+  `name` text NOT NULL,
+  `url` text NOT NULL,
+  `delay` int(11) NOT NULL,
+  `nextrun` int(11) NOT NULL,
+  PRIMARY KEY (`jobID`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `runs`
+--
+
+DROP TABLE IF EXISTS `runs`;
+CREATE TABLE IF NOT EXISTS `runs` (
+  `runID` bigint(20) NOT NULL AUTO_INCREMENT,
+  `job` int(11) NOT NULL,
+  `statuscode` char(3) NOT NULL,
+  `result` longtext NOT NULL,
+  `timestamp` int(11) NOT NULL,
+  PRIMARY KEY (`runID`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `userID` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `password` char(60) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  PRIMARY KEY (`userID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
